7d978632da96df8d9a33e4ab35f52ca3a23ac06d
vinay nagisetty <83533201+vinaynagisetty@users.noreply.github.com>
2024-06-12 05:07:18 +0000

    did the lazy loading using routing and changed the port number to 4229

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Suspense, lazy } from "react";
 
 import NavBar from "./NavBar"
 import CustomerList from "./customerList";
@@ -10,9 +10,10 @@ import TestUseEffect from "./testUseEffect";
 
 import TwoWayDataBinding from "./twoWayDataBinding";
 import ConditionalRendering from "./conditionalRendering";
-import Home from "./home";
-import About from "./about";
-import Login from "./loggin";
+// import Home from "./home";
+// import About from "./about";
+// import Login from "./loggin";
+
 import Logout,{ChganePassword,ChganeUserName} from "./logout";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PageNotFound from "./pageNotFound";
@@ -27,6 +28,11 @@ import Parent from "./chilfToParentCommParent";
 // import Form from "./form1";
 import AxiosExample from "./axiosExample1"
 import LazyLoading from "./lazyloading"
+
+let Home=lazy(()=>import("./home"));
+let About=lazy(()=>import("./about"));
+let Login=lazy(()=>import("./loggin"));
+
 export default class App extends Component {
   render() {
     return (
@@ -36,22 +42,24 @@ export default class App extends Component {
       // <About/>
       // </>
       //  <h1>ff</h1>
-        // <BrowserRouter>
-        //    <ConditionalRendering />
+        <BrowserRouter>
+           <ConditionalRendering />
+           <Suspense>
            
-        //     <Routes>
-        //          <Route exact path="/" element={<Home/>} />
+            <Routes>
+                 <Route exact path="/" element={<Home/>} />
                  
-        //          <Route path="/about" element={<About/>} />
-        //          <Route path="/login/:id/:custid" element={<Login/>} />
-        //          <Route path="/logOut" element={<Logout/>} >
-        //                 <Route path="chganePassword" element={<ChganePassword />} />
-        //                 <Route path="ChganeUserName" element={<ChganeUserName />} />
-        //           </Route>
-        //           <Route path="*" element={<PageNotFound />} />
+                 <Route path="/about" element={<About/>} />
+                 <Route path="/login/:id/:custid" element={<Login/>} />
+                 <Route path="/logOut" element={<Logout/>} >
+                        <Route path="chganePassword" element={<ChganePassword />} />
+                        <Route path="ChganeUserName" element={<ChganeUserName />} />
+                  </Route>
+                  <Route path="*" element={<PageNotFound />} />
                   
-        //     </Routes>
-        // </BrowserRouter>  //this is the example Routing ,nested routing and query and routeparameters exmaple
+            </Routes>
+            </Suspense> 
+        </BrowserRouter>  //this is the example Routing ,nested routing and query and routeparameters exmaple
         //  <UseRef />    //this is the example for use ref 
         // <CustomeHook />   //here i create custome hook for api calling
         // <PropsDrilling />   //here i did props drilling and useContext
@@ -61,7 +69,7 @@ export default class App extends Component {
         // <Parent />  //we are doing here todo list by sending data from child to parent
         // <Form /> //basic form example using useState and useRef
         // <AxiosExample />  //example of how to use axios library
-        <LazyLoading />
+        // <LazyLoading />  //lazy loading using conditional rendering
     );
   }
 }
